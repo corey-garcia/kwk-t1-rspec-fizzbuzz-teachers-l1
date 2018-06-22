1037a8e469eab2cb162683e2cf913e72028f5aab
Corey Garcia <corey.w.garcia@outlook.com>
2018-06-22 23:00:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(number)
-  if number % 3 == 0  "Fizz" end
-  if number % 5 == 0  "Buzz" end
-  if number % 15 == 0 "FizBuzz" end
+  if number % 3 == 0  then "Fizz" end
+  if number % 5 == 0  then "Buzz" end
+  if number % 15 == 0 then "FizBuzz" end
   return nil
 end
